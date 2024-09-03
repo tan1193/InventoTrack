ebfda6f00cc874a1c8f81e6036deee0147e91fcc
tan1193 <hoangtan1193@gmail.com>
2024-09-04 01:29:11 +0700

    add SUPABASE config and test config

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.get("/")
 
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/health")
+def health_check():
+    try:
+        # Attempt to fetch data from a table named 'test_table'
+        response = supabase.table("products").select("*").execute()
+        return response.data
+        # log the response
+        return {"status": "success", "message": "Successfully connected to Supabase"}
+  
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
